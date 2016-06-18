13caf01f0b8601084399a42b0a6eb1a6c1ebee4e
Xin Lin <Liiiiinnnnn@Xins-MacBook-Air.local>
2016-06-17 22:57:59 -0700

    basic skeleton

---
+++ itinerary.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Itinerary Generation</title>
+        <!-- stylesheets -->
+		<link rel="stylesheet" type="text/css" href="css/hack.css">
+		<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+        <?php include "php/php_component.php";?>
+        
+    </head>
+    
+    <body>
+        <div id="frame">
+            <div id="top">
+            </div><!-- End top div -->
+            
+            <div id="main">       
+                <div id="content">
+                    <p>Placeholder: Auto-Generated Itinerary</p>
+                </div><!-- End content div -->
+ 
+            </div><!-- end main div -->
+            <div id="bottom">
+                <p>&copy; 2016 by Xin Lin, Yiting Wang, Ji hun Kim, Juhee Lee</p>
+            </div>
+        </div><!-- end frame div -->
+    </body>
+    
+</html>
