d85a1cd02f2931765a8cee283ace7adcb4f01d05
Ju Hee Lee <juhee0202@gmail.com>
2016-06-18 06:05:07 -0700

    edited sample

--- backend/main.py
+++ backend/main.py
@@ -8,18 +8,29 @@
 # form = cgi.FieldStorage()
 # print json.dumps({"key": form.getValue("data")})
 import calendar 
+import numpy as np
 from mod_python import apache 
+
 def getMonth(req,month): 
 	req.write(calendar.month(2005, int(month),2,3))
 
 
-def createItinerary(start, matrix):
+def createItinerary(start, start_time, matrix):
+	# if 12 < start_time < 1:
+	# 	start at a restaurant
+	# if 6 < start_time < 7:
+	# 	start at a restaurant
+	# else:
+	# 	start at a sight-seeing place
+
+	travel_times = matrix[start]
+	closest_neighbor = min(travel_times, key=travel_times.get)
+
 	
 
+
 def main():
-	# searchByLocation('Seattle')
-	# searchByKeywords('Seattle', 'japanese food')
-	searchByLocation(47.6205,-122.3493)
+	
 
 if __name__ == "__main__":
 	main()
