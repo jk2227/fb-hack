692deeb7c39833c7f498ea3064dc06b835155731
Xin Lin <Liiiiinnnnn@Xins-MacBook-Air.local>
2016-06-17 22:44:41 -0700

    initial files

---
+++ home.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Itinerary Generation</title>
+        <!-- stylesheets -->
+		<link rel="stylesheet" type="text/css" href="css/hack.css">
+		<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+        <?php include "component/php_component.php";?>
+        
+    </head>
+    
+    <body>
+        <div id="frame">
+            <div id="top">
+                <!-- navigation -->
+                <div id="nav">
+                    <ul>
+<?php get_nav();?>
+                    </ul>
+                </div><!-- end nav  div -->
+            </div><!-- End top div -->
+            
+            <div id="main">
+                <div id="image">
+                    Placeholder: background image
+                </div>
+                
+                <div id="content">
+                    Placeholder: textbox Enter Destination
+                    Submit
+                </div><!-- End content div -->
+ 
+            </div><!-- end main div -->
+            <div id="bottom">
+                <p>&copy; 2016 by Xin Lin, Yiting Wang, Ji hun Kim, Juhee Lee</p>
+            </div>
+        </div><!-- end frame div -->
+    </body>
+    
+</html>
